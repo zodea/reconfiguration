468a5db2ed146d89aea12a6b5306d162d7053b55
zodea <zd929560638@gmail.com>
2022-01-09 22:52:01 +0800

    perf: use declared function usd and move the duplicated division by 100 into the function to replace format function variable

--- src/chapter1/statement.js
+++ src/chapter1/statement.js
@@ -2,18 +2,13 @@ module.exports = function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
-  const format = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-  }).format;
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
     // print line for this order
-    result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     totalAmount += amountFor(perf);
   }
-  result += `Amount owed is ${format(totalAmount / 100)}\n`;
+  result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   function amountFor(aPerformance) {
     let result = 0;
@@ -44,6 +39,13 @@ module.exports = function statement(invoice, plays) {
     result += Math.max(perf.audience - 30, 0);
     if ("comedy" === playFor(perf).type) result += Math.floor(perf.audience / 5);
     return result;
+  }
+  function usd(aNumber) {
+    return new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+      minimumFractionDigits: 2,
+    }).format(aNumber / 100);
   }
   return result;
 };
