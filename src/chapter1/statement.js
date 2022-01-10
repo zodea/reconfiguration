44ef83c65165a7a21a15ca493a1317ca44448d22
zodea <zd929560638@gmail.com>
2022-01-10 21:55:33 +0800

    perf: move function volumeCreditsFor too

--- src/chapter1/statement.js
+++ src/chapter1/statement.js
@@ -7,6 +7,7 @@ module.exports = function statement(invoice, plays) {
     const result = Object.assign({}, aPerformance);
     result.play = playFor(result);
     result.amount = amountFor(result);
+    result.volumeCredits = volumeCreditsFor(result);
     return result;
   }
   function playFor(aPerformance) {
@@ -33,6 +34,12 @@ module.exports = function statement(invoice, plays) {
     }
     return result;
   }
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
+    return result;
+  }
 };
 
 function renderPlainText(data, _plays) {
@@ -44,12 +51,6 @@ function renderPlainText(data, _plays) {
   result += `Amount owed is ${usd(totalAmount())}\n`;
   result += `You earned ${totalVolumeCredits()} credits\n`;
   return result;
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
-    return result;
-  }
   function usd(aNumber) {
     return new Intl.NumberFormat("en-US", {
       style: "currency",
@@ -60,7 +61,7 @@ function renderPlainText(data, _plays) {
   function totalVolumeCredits() {
     let volumeCredits = 0;
     for (let perf of data.performances) {
-      volumeCredits += volumeCreditsFor(perf);
+      volumeCredits += perf.volumeCredits;
     }
     return volumeCredits;
   }
