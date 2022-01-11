6d8bd79f4799021abafe8d04e3224bf306600f51
zodea <zd929560638@gmail.com>
2022-01-11 23:02:03 +0800

    perf: 1.9 look the all code

--- src/chapter1/createStatementData.js
+++ src/chapter1/createStatementData.js
@@ -6,18 +6,6 @@ export default function createStatementData(invoice, plays) {
   result.totalVolumeCredits = totalVolumeCredits(result);
   return result;
 
-  // 构造函数无法返回子类，所以通过工厂函数来实现
-  function createPerformanceCaculator(aPerformance, aPlay) {
-    switch (aPlay.type) {
-      case "tragedy":
-        return new TragedyCalculator(aPerformance, aPlay);
-      case "comedy":
-        return new ComedyCalculator(aPerformance, aPlay);
-      default:
-        throw new Error(`unknown type: ${aPlay.type}`);
-    }
-  }
-
   function enrichPerformance(aPerformance) {
     // 由于在此处直接调用了几个计算函数，因此由这里开始创建一个类来做后续操作
     const calculator = createPerformanceCaculator(aPerformance, playFor(aPerformance));
@@ -38,9 +26,21 @@ export default function createStatementData(invoice, plays) {
   }
 }
 
+// 构造函数无法返回子类，所以通过工厂函数来实现
+function createPerformanceCaculator(aPerformance, aPlay) {
+  switch (aPlay.type) {
+    case "tragedy":
+      return new TragedyCalculator(aPerformance, aPlay);
+    case "comedy":
+      return new ComedyCalculator(aPerformance, aPlay);
+    default:
+      throw new Error(`unknown type: ${aPlay.type}`);
+  }
+}
+
 class PerformanceCalculator {
   constructor(aPerformance, aplay) {
-    this.aPerformance = aPerformance;
+    this.performance = aPerformance;
     this.play = aplay;
   }
 
@@ -49,15 +49,15 @@ class PerformanceCalculator {
   }
 
   get volumeCredits() {
-    return Math.max(this.aPerformance.audience - 30, 0);
+    return Math.max(this.performance.audience - 30, 0);
   }
 }
 
 class TragedyCalculator extends PerformanceCalculator {
   get amount() {
     let result = 40000;
-    if (this.aPerformance.audience > 30) {
-      result += 1000 * (this.aPerformance.audience - 30);
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30);
     }
     return result;
   }
@@ -65,14 +65,14 @@ class TragedyCalculator extends PerformanceCalculator {
 class ComedyCalculator extends PerformanceCalculator {
   get amount() {
     let result = 30000;
-    if (this.aPerformance.audience > 20) {
-      result += 10000 + 500 * (this.aPerformance.audience - 20);
+    if (this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20);
     }
-    result += 300 * this.aPerformance.audience;
+    result += 300 * this.performance.audience;
     return result;
   }
 
   get volumeCredits() {
-    return super.volumeCredits + Math.floor(this.aPerformance.audience / 5);
+    return super.volumeCredits + Math.floor(this.performance.audience / 5);
   }
 }
